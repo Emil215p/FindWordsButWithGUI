d4ac6cf27a74ff0e685432109ab0386542f1d3d0
Ninegone <Kevi2643@edu.sde.dk>
2023-01-18 12:23:32 +0100

    f

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,9 +25,14 @@ namespace FindWordsButWithGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
+
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
