247d7bd181058f05dfb989264be24fcf876dee5f
Emil214p <emil215p@edu.sde.dk>
2023-01-18 08:54:53 +0100

    Added UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FindWordsButWithGUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
