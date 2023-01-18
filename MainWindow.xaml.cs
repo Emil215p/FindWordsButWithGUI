aab18aee90d3ac5633494fadca7a83ed49e4357a
Emil214p <emil215p@edu.sde.dk>
2023-01-18 10:32:28 +0100

    Mere kode addet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UniqueLengthAmountWords;
 
 namespace FindWordsButWithGUI
 {
@@ -28,11 +31,45 @@ namespace FindWordsButWithGUI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+
+
+
+
+            int a, b;
+            try
+            {
+                a = Convert.ToInt32(LetterAmount.Text);
+                b = Convert.ToInt32(WordAmount.Text);
+            } catch(Exception ex)
+            {
+                MessageBox.Show("Invalid numbers.");
+            }
+
+            ReadSearch.Length = 5;
+            ReadSearch.AmountOfWords = 5;
+            ReadSearch.Clear();
+            ReadSearch.ReadFile(filepath);
+
+            try
+            {
+                ReadSearch.SearchClever();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        string filepath;
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            bool? response = ofd.ShowDialog();
+            if (response == true)
+            {
+                filepath = ofd.FileName;
+            }
         }
     }
 }
